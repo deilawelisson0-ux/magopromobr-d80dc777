8bbb45d7b1c86b6b0f27596d79def8546ea905ad
deilawelisson0-ux <deilawelisson0@gmail.com>
2026-03-24 02:55:25 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,21 @@
 import { lazy, Suspense } from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Index from "./pages/Index.tsx";
 
 const NotFound = lazy(() => import("./pages/NotFound.tsx"));
 
 const App = () => (
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Index />} />
-      <Route
-        path="*"
-        element={
-          <Suspense fallback={null}>
-            <NotFound />
-          </Suspense>
-        }
-      />
-    </Routes>
-  </BrowserRouter>
+  <Routes>
+    <Route path="/" element={<Index />} />
+    <Route
+      path="*"
+      element={
+        <Suspense fallback={null}>
+          <NotFound />
+        </Suspense>
+      }
+    />
+  </Routes>
 );
 
 export default App;
