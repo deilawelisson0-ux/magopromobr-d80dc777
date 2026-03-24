2958562fba5d3c86d36f0e6275b3c7132cd49382
deilawelisson0-ux <deilawelisson0@gmail.com>
2026-03-24 01:45:24 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from "vite";
+import { base: "/magopromobr-d80dc777/", } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
